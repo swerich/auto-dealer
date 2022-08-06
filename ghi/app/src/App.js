9f425402c5cc5f04226665548c62c78a22d1386f
Richard Scott <rich5d@icloud.com>
2022-08-05 17:42:53 -0700

    the final submission

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -7,19 +7,16 @@ import AutomobileForm from './AutomobileForm';
 import NewVehicleModelForm from './NewVehicleModelForm';
 import AutomobileList from './AutomobileList';
 import ManufacturerForm from './ManufacturerForm';
-<<<<<<< HEAD
-import SaleList from './SaleList'
+import AppointmentsList from './AppointmentsList';
+import AppointmentForm from './AppointmentForm';
+import ServiceHistory from './ServiceHistory';
+import NewTechnicianForm from './NewTechnicianForm';
+import SaleList from './SaleList';
 import SalesPersonForm from './SalesPersonForm';
 import SaleRecordForm from './SaleRecordForm';
 import NewCustomerForm from './NewCustomerForm';
 import SalesPersonHistory from './SalesPersonHistory';
 
-=======
-import AppointmentsList from './AppointmentsList';
-import AppointmentForm from './AppointmentForm';
-import ServiceHistory from './ServiceHistory';
-import NewTechnicianForm from './NewTechnicianForm';
->>>>>>> 79599e38c02a9d3e5d7f88c97a0b5be00dc71dcc
 
 function App() {
   return (
@@ -34,27 +31,14 @@ function App() {
           <Route path="/automobiles" element={<AutomobileList />} />
           <Route path="/manufacturers/new" element={<ManufacturerForm />} />
           <Route path="/manufacturers" element={<ManufacturerList />} />
-<<<<<<< HEAD
           <Route path="/sales/new" element={<SaleRecordForm />} />
           <Route path="/customers/new" element={<NewCustomerForm />} />
           <Route path="/sales_people" element={<SalesPersonHistory />} />
 
 
-
-
-
-
-          
-
-
-
-
-
-=======
           <Route path="/appointments" element={<AppointmentsList />} />
           <Route path="/appointments/new" element={<AppointmentForm />} />
           <Route path="/service_history" element={<ServiceHistory />} />
->>>>>>> 79599e38c02a9d3e5d7f88c97a0b5be00dc71dcc
           <Route path="/models" element={<VehicleModels />} />
           <Route path="/models/new" element={<NewVehicleModelForm />} />
           <Route path="/technicians" element={<NewTechnicianForm />} />
